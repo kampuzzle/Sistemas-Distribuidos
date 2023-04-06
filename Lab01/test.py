1e23058c530e29f6a52ee9c4eb666565d1f86612
lorenzuou <lorenzoscaramussa@hotmail.com>
2023-04-06 11:05:54 +0000

    mais um

--- Lab01/test.py
+++ Lab01/test.py
@@ -16,28 +16,28 @@ def generate_random_vector(size):
 
 if __name__ == '__main__':
     vector_sizes = [14, 100, 1000, 10000, 10000]
-    thread_counts = [1, 2, 4, 6, 8,10,12,14]
+    process_counts = [1, 2, 4, 6, 8,10,12,14,16]
 
    
-    with open('data/results_2.csv', 'w', newline='') as csvfile:
+    with open('data/results.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(["size", "thread_num", "time"])
+        writer.writerow(["size", "process_num", "time"])
 
         for size in vector_sizes:
-            print(f"Sorting vector of size {size} with {thread_counts} threads")
+            print(f"Sorting vector of size {size} with {process_counts} process")
            
-            for thread_count in thread_counts:
-                print(f"Sorting vector of size {size} with {thread_count} threads")
-                if(size == 100000 and thread_count <6): 
+            for process_count in process_counts:
+                print(f"Sorting vector of size {size} with {process_count} process")
+                if(size == 100000 and process_count <6): 
                     continue
                     
                 vector = generate_random_vector(size)
 
                 start_time = time.time()
-                sorted_vector = sort_vector(vector, thread_count)
+                sorted_vector = sort_vector(vector, process_count)
                 end_time = time.time()
 
                
-                writer.writerow([size, thread_count, end_time - start_time])
+                writer.writerow([size, process_count, end_time - start_time])
 
                 sys.stdout.flush()  
