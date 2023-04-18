1443d5bae64524ebf755c1e69d8cd472d075d34b
asidjasi <aisdjiasd>
2023-04-18 18:42:10 -0300

    fdf

--- Lab02/server.py
+++ Lab02/server.py
@@ -1,20 +1,40 @@
 import flwr as fl
 from flwr.server.strategy import FedAvg
-from flwr.server.server import start_server
+from flwr.server import start_server
+
+from cliente import MyClient
+
+
+def weighted_average(metrics):
+    # Multiply accuracy of each client by number of examples used
+    acc = [num_examples * m["accuracy"] for num_examples, m in metrics]
+    examples = [num_examples for num_examples, _ in metrics]
+
+    # Aggregate and return custom metric (weighted average)
+    results = {"accuracy": sum(acc) / sum(examples)}
+    return results
 
 if __name__ == "__main__":
     # Define strategy
-    strategy = FedAvg()
-
-    # Define server config
-    server_config = fl.server.ServerConfig(
-        grpc_max_message_length=1024 * 1024 * 1024,
-        num_rounds=10,
-        num_clients=5,
-        sample_fraction=0.1,
-        min_sample_size=5,
-        min_num_clients=2
+   # Create FedAvg strategy
+    strategy = fl.server.strategy.FedAvg(
+  
+        min_evaluate_clients=5,  
+       
+        evaluate_metrics_aggregation_fn=weighted_average,
     )
 
+
+    server_address = "[::]:8080"
     # Start Flower server
-    start_server(config=server_config, strategy=strategy)
+    server = start_server(server_address=server_address,config=fl.server.ServerConfig(num_rounds=1), strategy=strategy)
+
+    #print("Server started at", server_address)
+
+    #print metrics of each round
+    for i in range(1):
+        print(server.get_metrics())
+
+    # save model
+    model = server.get_model()
+    model.save("model.h5")
