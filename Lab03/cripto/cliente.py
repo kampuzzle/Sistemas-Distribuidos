a4c1bb1f514584c0eb2c46bc91c3139ef2629783
lmscaramussa <lorenzoscaramussa@hotmail.com>
2023-04-25 18:38:47 -0300

    lab3

---
+++ Lab03/cripto/cliente.py
@@ -0,0 +1,96 @@
+import grpc
+import mineracao_pb2
+import mineracao_pb2_grpc
+import threading
+
+
+def connect():
+    channel = grpc.insecure_channel('localhost:8080')
+    stub = mineracao_pb2_grpc.apiStub(channel)
+    print("Stub created!")
+    return stub
+
+
+def get_current_transaction(stub):
+    response = stub.getTransactionId(mineracao_pb2.void())
+    print("Current transaction ID: ", response.result)
+
+
+def get_challenge(stub):
+    transaction_id = input("Enter transaction ID: ")
+    response = stub.getChallenge(mineracao_pb2.transactionId(transactionId=int(transaction_id)))
+    print("Challenge: ", response.result)
+
+
+def get_transaction_status(stub):
+    transaction_id = input("Enter transaction ID: ")
+    response = stub.getTransactionStatus(mineracao_pb2.transactionId(transactionId=int(transaction_id)))
+    print("Transaction status: ", response.result)
+
+
+def get_winner(stub):
+    transaction_id = input("Enter transaction ID: ")
+    response = stub.getWinner(mineracao_pb2.transactionId(transactionId=int(transaction_id)))
+    print("Winner: ", response.result)
+
+
+def get_solution(stub):
+    transaction_id = input("Enter transaction ID: ")
+    response = stub.getSolution(mineracao_pb2.transactionId(transactionId=int(transaction_id)))
+    print("Solution: ", response.result)
+
+
+def mine(stub):
+    current_transaction = stub.getTransactionId(mineracao_pb2.void()).result
+    challenge = stub.getChallenge(mineracao_pb2.transactionId(transactionId=current_transaction)).result
+    threads = []
+    for i in range(4):  # 4 threzzads para processamento paralelo
+        t = threading.Thread(target=mine_challenge, args=(i, challenge, stub))
+        threads.append(t)
+        t.start()
+    for t in threads:
+        t.join()
+
+
+def mine_challenge(thread_id, challenge, stub):
+    print("Thread ", thread_id, " started")
+    for i in range(1000000):  # laço de processamento simulando solução de desafio
+        solution = i ^ challenge
+        if solution % 100000 == 0:
+            print("Thread ", thread_id, " at ", i)
+        if solution % 777 == 0:  # solução encontrada
+            print("Thread ", thread_id, " found solution ", solution)
+            break
+    else:
+        print("Thread ", thread_id, " did not find solution")
+        return
+    # submeter solução ao servidor
+    response = stub.submitChallenge(
+        mineracao_pb2.challengeArgs(transactionId=int(current_transaction), clientId=thread_id, seed=solution))
+    print("Thread ", thread_id, " server response: ", response.result)
+
+
+def menu(stub):
+    while True:
+        print("Choose an option:")
+        print("1. Get current transaction ID")
+        print("2. Get challenge")
+        print("3. Get transaction status")
+        print("4. Get winner")
+        print("5. Get solution")
+        print("6. Mine")
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            get_current_transaction(stub)
+        elif choice == "2":
+            get_challenge(stub)
+        elif choice == "3":
+            get_transaction_status(stub)
+        elif choice == "4":
+            get_winner(stub)
+        elif choice == "5":
+            get_solution(stub)
+
+if __name__ == '__main__':
+    stub = connect()
+    menu(stub)
---
+++ Lab03/cripto/servidor.py
@@ -0,0 +1,73 @@
+from concurrent import futures
+import time
+import grpc
+import mineracao_pb2
+import mineracao_pb2_grpc
+import hashlib
+
+
+class CryptoMiningServiceServicer(mineracao_pb2_grpc.apiServicer):
+
+    def __init__(self):
+        self.transactions = {}
+
+    def getTransactionID(self, request, context):
+        transaction_id = max(self.transactions.keys()) + 1 if self.transactions else 1
+        self.transactions[transaction_id] = {'challenge': None, 'solution': None, 'winner': None}
+        return mineracao_pb2.Transaction(transaction_id=transaction_id)
+
+    def getChallenge(self, request, context):
+        if request.transaction_id not in self.transactions:
+            return mineracao_pb2.Transaction(challenge=-1)
+        elif self.transactions[request.transaction_id]['challenge'] is None:
+            return mineracao_pb2.Transaction(challenge=0)
+        else:
+            return mineracao_pb2.Transaction(challenge=self.transactions[request.transaction_id]['challenge'])
+
+    def getTransactionStatus(self, request, context):
+        if request.transaction_id not in self.transactions:
+            return mineracao_pb2.Transaction(winner=-1)
+        elif self.transactions[request.transaction_id]['solution'] is None:
+            return mineracao_pb2.Transaction(winner=1)
+        else:
+            return mineracao_pb2.Transaction(winner=self.transactions[request.transaction_id]['winner'])
+
+    def submitChallenge(self, request, context):
+        if request.transaction_id not in self.transactions:
+            return mineracao_pb2.Transaction(winner=-1)
+        elif self.transactions[request.transaction_id]['solution'] is not None:
+            return mineracao_pb2.Transaction(winner=0)
+        else:
+            hash_object = hashlib.sha1(str(request.challenge).encode())
+            hex_dig = hash_object.hexdigest()
+            if hex_dig[:5] == '00000':
+                self.transactions[request.transaction_id]['solution'] = request.solution
+                self.transactions[request.transaction_id]['winner'] = request.winner
+                return mineracao_pb2.Transaction(winner=2)
+            else:
+                return mineracao_pb2.Transaction(winner=1)
+
+    def getWinner(self, request, context):
+        if request.transaction_id not in self.transactions:
+            return mineracao_pb2.Transaction(winner=-1)
+        else:
+            return mineracao_pb2.Transaction(winner=self.transactions[request.transaction_id]['winner'])
+
+    def getSolution(self, request, context):
+        if request.transaction_id not in self.transactions:
+            return mineracao_pb2.SolutionResult(status=-1)
+        else:
+            status = 0 if self.transactions[request.transaction_id]['solution'] else 1
+            return mineracao_pb2.SolutionResult(status=status, solution=self.transactions[request.transaction_id]['solution'], challenge=self.transactions[request.transaction_id]['challenge'])
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    mineracao_pb2_grpc.add_apiServicer_to_server(CryptoMiningServiceServicer(), server)
+    server.add_insecure_port('[::]:8080')
+    server.start()
+    
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    serve()
