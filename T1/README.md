f01b144735dfb721d7f3f428d69e80440e6fef9e
lmscaramussa <lorenzoscaramussa@hotmail.com>
2023-05-18 16:49:00 -0300

    sdad

---
+++ T1/README.md
@@ -0,0 +1,5 @@
+## Instruções para execução do código 
+
+Primeiro, é necessário criar os dados de treino e teste executando o script dentro de scritps/divisor_dataset.py
+
+Agora, execute o server.py. Em outros terminais, execute os clientes no formato: "python3 cliente.py X", onde X é o numero do cliente, que sera usado para escolher o dataset
--- T1/scripts/divisor_dataset.py
+++ T1/scripts/divisor_dataset.py
@@ -23,8 +23,8 @@ y_train = np.eye(10)[y_train]
 y_test = np.eye(10)[y_test]
 
 # Dividir os dados de treino em três conjuntos diferentes
-x_train_1, x_train_2, x_train_3 = np.split(x_train, 3)
-y_train_1, y_train_2, y_train_3 = np.split(y_train, 3)
+x_train_1, x_train_2, x_train_3, x_train_4, x_train_5 = np.split(x_train, 5)
+y_train_1, y_train_2, y_train_3, y_train_4, y_train_5 = np.split(y_train, 5)
 
 # Criar as pastas de treino e teste se não existirem
 os.makedirs("../treino", exist_ok=True)
@@ -37,6 +37,10 @@ np.save("../treino/x_train_2.npy", x_train_2)
 np.save("../treino/y_train_2.npy", y_train_2)
 np.save("../treino/x_train_3.npy", x_train_3)
 np.save("../treino/y_train_3.npy", y_train_3)
+np.save("../treino/x_train_4.npy", x_train_4)
+np.save("../treino/y_train_4.npy", y_train_4)
+np.save("../treino/x_train_5.npy", x_train_5)
+np.save("../treino/y_train_5.npy", y_train_5)
 
 # Salvar os dados de teste em arquivos numpy na pasta de teste
 np.save("../teste/x_test.npy", x_test)
