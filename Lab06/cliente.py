ae53755071ca6a0194a2d952529c7c37e0079dce
lmscaramussa <lorenzoscaramussa@hotmail.com>
2023-05-23 18:28:22 -0300

    progresoo

--- Lab06/cliente.py
+++ Lab06/cliente.py
@@ -1,45 +1,78 @@
 import json
 import paho.mqtt.client as mqtt
+import hashlib
+
+class Cliente:
+    def __init__(self, broker_address, broker_port):
+        self.broker_address = broker_address
+        self.broker_port = broker_port
+        
+        # Cria um cliente MQTT
+        self.client = mqtt.Client()
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+
+    # Callback para quando o cliente se conecta ao broker
+    def on_connect(self, client, userdata, flags, rc):
+        print("Conectado ao broker MQTT")
+        self.client.subscribe("sd/challenge")
+
+    # Callback para quando o cliente recebe uma mensagem
+    def on_message(self, client, userdata, msg):
+        topic = msg.topic
+        payload = msg.payload.decode("utf-8")
+        
+        if topic == "sd/challenge":
+            self.processar_desafio(payload)
+
+    # Envia uma solução para o controlador
+    def enviar_solucao(self, client_id, transaction_id, solution):
+        data = {
+            "ClientID": client_id,
+            "TransactionID": transaction_id,
+            "Solution": solution
+        }
+        payload = json.dumps(data)
+        self.client.publish("sd/solution", payload)
+
+    # Processa um desafio recebido do controlador
+    def processar_desafio(self, payload):
+        data = json.loads(payload)
+        transaction_id = data["TransactionID"]
+        challenge = data["Challenge"]
+        
+        # Resolva o desafio
+        solution = self.resolver_desafio(challenge)
+        
+        # Envie a solução para o controlador
+        client_id = 123  # Substitua pelo ID do cliente real
+        self.enviar_solucao(client_id, transaction_id, solution)
+
+    # Resolve um desafio criptográfico
+    def resolver_desafio(self, challenge):
+        # Implemente a lógica para resolver o desafio aqui
+        # Neste exemplo, usamos o SHA-1 para calcular a solução
+        challenge_str = str(challenge)
+        solution = hashlib.sha1(challenge_str.encode()).hexdigest()
+        return solution
+
+    # Inicia o cliente
+    def start(self):
+        # Conecta ao broker MQTT
+        self.client.connect(self.broker_address, self.broker_port, 60)
+        
+        # Inicia o loop de rede do cliente MQTT
+        self.client.loop_start()
+
+        
+
 
 # Configurações do broker MQTT
-broker_address = "localhost"  # Endereço do broker MQTT (por exemplo, EMQX)
+broker_address = "broker.emqx.io"  # Endereço do broker MQTT (por exemplo, EMQX)
 broker_port = 1883  # Porta do broker MQTT
 
-# Callback para quando o cliente se conecta ao broker
-def on_connect(client, userdata, flags, rc):
-    print("Conectado ao broker MQTT")
-    client.subscribe("sd/solution")  # Subscreve no tópico de soluções
+# Cria uma instância do cliente
+cliente = Cliente(broker_address, broker_port)
 
-# Callback para quando o cliente recebe uma mensagem do broker
-def on_message(client, userdata, msg):
-    topic = msg.topic
-    payload = msg.payload.decode("utf-8")
-    data = json.loads(payload)
-    client_id = data["ClientID"]
-    transaction_id = data["TransactionID"]
-    solution = data["Solution"]
-    
-    # Processar a solução recebida
-    # ...
-
-# Cria um cliente MQTT
-client = mqtt.Client()
-
-# Define as callbacks
-client.on_connect = on_connect
-client.on_message = on_message
-
-# Conecta ao broker MQTT
-client.connect(broker_address, broker_port, 60)
-
-# Inicia o loop de rede do cliente MQTT
-client.loop_start()
-
-# Mantém o cliente em execução até ser interrompido
-try:
-    while True:
-        pass
-except KeyboardInterrupt:
-    client.loop_stop()
-    client.disconnect()
-    print("Cliente MQTT desconectado")
+# Inicia o cliente
+cliente.start()
