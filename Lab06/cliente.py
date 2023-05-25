ce0e4eb501ff3c5030a88455cda4f32298b8a521
lorenzuou <lorenzoscaramussa@hotmail.com>
2023-05-24 21:43:16 -0300

    dsd

--- Lab06/cliente.py
+++ Lab06/cliente.py
@@ -1,19 +1,27 @@
 import json
 import paho.mqtt.client as mqtt
 import hashlib
+import random
+
 
 class Cliente:
-    def __init__(self, broker_address, broker_port):
+    def __init__(self, broker_address):
         self.broker_address = broker_address
-        self.broker_port = broker_port
+ 
         
         # Cria um cliente MQTT
         self.client = mqtt.Client()
+        self.id_client = random.randint(0, 1000)
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
 
+
+
     # Callback para quando o cliente se conecta ao broker
     def on_connect(self, client, userdata, flags, rc):
+        if rc != 0:
+            print("Falha na conexão. Código de retorno:", rc)
+            exit()
         print("Conectado ao broker MQTT")
         self.client.subscribe("sd/challenge")
 
@@ -59,20 +67,19 @@ class Cliente:
     # Inicia o cliente
     def start(self):
         # Conecta ao broker MQTT
-        self.client.connect(self.broker_address, self.broker_port, 60)
-        
+        self.client.connect(self.broker_address)
         # Inicia o loop de rede do cliente MQTT
         self.client.loop_start()
 
         
+if __name__ == "__main__":
 
+    # Configurações do broker MQTT
+    broker_address = "broker.emqx.io"  # Endereço do broker MQTT (por exemplo, EMQX)
+    broker_port = 1883  # Porta do broker MQTT
 
-# Configurações do broker MQTT
-broker_address = "broker.emqx.io"  # Endereço do broker MQTT (por exemplo, EMQX)
-broker_port = 1883  # Porta do broker MQTT
+    # Cria uma instância do cliente
+    cliente = Cliente(broker_address)
 
-# Cria uma instância do cliente
-cliente = Cliente(broker_address, broker_port)
-
-# Inicia o cliente
-cliente.start()
+    # Inicia o cliente
+    cliente.start()
